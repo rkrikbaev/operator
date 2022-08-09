c35b57ce294ca9cb1b6f41ca78be35c63d527740
Rustam Krikbaev <r.krikbaev@zeinetsse.com>
2022-08-09 10:34:40 +0600

    addd

--- app/service.py
+++ app/service.py
@@ -27,8 +27,8 @@ class ModelAsHTTPService():
                     }
         try:
             # os.system("""'curl --location --request POST 'http://almaty2:8005/action' --header 'Content-Type: application/json' --data-raw '{"data": [[1626321114000],[1626321115000],[1626321116000]]}'""")
-            # result = requests.request('POST',url='http://almaty2:8005/action',headers={'Content-Type': 'application/json'},data=json.dumps({'data':payload}))
-            result = requests.request('POST', url=url, headers={'Content-Type': 'application/json'}, data=json.dumps({'data':payload}))
+            result = requests.request('POST',url='http://almaty2:8005/action',headers={'Content-Type': 'application/json'},data=json.dumps({'data':payload}))
+            # result = requests.request('POST', url=url, headers={'Content-Type': 'application/json'}, data=json.dumps({'data':payload}))
 
             response["finish_time"] = str(datetime.datetime.now())
             response['response'] = result.json()
