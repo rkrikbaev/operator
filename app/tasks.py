63f87ae9ba29cb707fb888dd5976b31571ea9668
Rustam Krikbaev <r.krikbaev@zeinetsse.com>
2022-08-11 18:02:16 +0600

    add

--- app/tasks.py
+++ app/tasks.py
@@ -18,17 +18,9 @@ service = ModelAsHTTPService()
 
 @app.task
 def predict(service_config, payload, point, model_id, model_features, regressor_names):
-    
-    tracking_server=os.environ.get('TRACKING_SERVER')
-    logger.debug(tracking_server)
 
     port = service_config.get('port')
     logger.debug('try to create container with model')
-
-    if tracking_server:
-        pass
-    else:
-        raise RuntimeError('Address of tracking server was not set')
 
     container_id, state = docker_engine.deploy_container(
         point, 
@@ -36,7 +28,7 @@ def predict(service_config, payload, point, model_id, model_features, regressor_
         model_features,
         model_id,
         regressor_names,
-        tracking_server=tracking_server
+        tracking_server='http://mlflow:5000'
         )
 
     logger.debug(f'Container state {container_id}')
